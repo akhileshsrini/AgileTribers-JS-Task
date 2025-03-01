018906a8d47dfed3393b1ec5c27bb50272124ff9
akhileshsrini <akhileshsrini23@gmail.com>
2025-03-01 08:43:22 +0530

    Added files via upload

---
+++ Task-1.js
@@ -0,0 +1,55 @@
+//Exercise 1
+console.log("Hello, World!!!");
+
+//Exercise 2
+console.log("Hello, Akhilesh");
+
+//Exercise 3
+console.log("Hello, Akhilesh");
+console.log("You are 19 years old");
+
+//Exercise 4
+var ex4_name = "Akhilesh";
+var ex4_age = 19;
+var ex4_isStudent = true;
+
+//Exercise 5 & 6
+var ex5_name = "Akhilesh";
+var ex5_age = 19;
+console.log("Hello, "+ex5_name+". You are "+ex5_age+" Years old");
+
+//Exercise 7
+console.log("Mathametical Operation b/w 7 and 5");
+console.log("7 + 5 = "+(7+5));
+console.log("7 - 5 = "+(7-5));
+console.log("7 x 5 = "+(7*5));
+console.log("7 / 5 = "+(7/5));
+
+//Exercise 8
+var vowels = ["a", "e", "i", "o", "u"];
+vowels.forEach(i => {
+    console.log(i)
+});
+
+//Exercise 9
+for (let i = 1; i < 5; i++) {
+    console.log("* ".repeat(i));
+}
+
+//Exercise 10, 11
+var ex10_name = "Akhilesh";
+var ex10_age = 19;
+var ex10_isStudent = true;
+ex10_name = "Akhilesh S";
+ex10_age = 20;
+ex10_isStudent = false;
+console.log("Hello, "+ex10_name+". You are "+ex10_age+" years old. Your student status is : "+ex10_isStudent);
+
+//Exercise 12
+let ex12_name = "Akhilesh";
+let ex12_age = 19;
+let ex12_dob = "22/02/2006";
+let ex12_height = 172.5;
+let ex12_weight = 75.8;
+let ex12_degree = "92 `C";
+let ex12_gender = "Male";
